c2a5bb80584252d327bbe3940e5f3fa13af1ffed
Tobias Andersson <tobiaserikandersson@gmail.com>
2024-12-02 16:38:39 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/actions_coverage/',
   plugins: [react()],
   test: {
     globals: true,
